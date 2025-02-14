d116cdda43a36cd375f084814e4f5d01748689db
Semyon <79764144+semyon0101@users.noreply.github.com>
2025-02-14 19:45:03 +0300

    0.4

--- Technofilm/db.py
+++ Technofilm/db.py
@@ -1,7 +1,21 @@
+import csv
+_films=[]
+with open('films.csv') as f:
+    reader = csv.reader(f)
+    try:
+        for row in reader:
+                _films.append(row)
+    except:
+        pass
+
 class Film:
     def __init__(self, id):
         self.id = id
-        self.name = "Blitz"
-        self.image = '//cfm.yidio.com/images/movie/251265/poster-180x270.jpg'
-        self.description =" In World War II London, nine-year-old George is evacuated to the countryside by his mother, Rita, to escape the bombings. Defiant and determined to return to his family, George embarks on an epic, perilous journey back home as Rita searches for him. Blitz is a 2024 drama with a runtime of 2 hours. It has received mostly positive reviews from critics and viewers, who have given it an IMDb score of 6.2 and a MetaScore of 71. "
-        self.creationYear = "2024"
+        self.find()
+
+    
+    def find(self):
+        self.name = _films[self.id+1][1]
+        self.image = _films[self.id+1][2]
+        self.description =_films[self.id+1][3]
+        self.creationYear = _films[self.id+1][4]
